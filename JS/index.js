770ea14834fe17f351eada34532e31c40bab5723
Rogier van Oudheusden <rogerthat@live.nl>
2021-12-08 13:02:31 +0100

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,30 @@
+let name = ('Roger')
+console.log(name)
+
+let x = 4;         
+let y = 4;         
+let z = x + y;
+console.log(z)
+
+let a = 4;         
+let b = 4;         
+let c = a - b;
+console.log(c)
+
+let d = 4;         
+let e = 4;         
+let f = d * e;
+console.log(f)
+
+let g = 4;         
+let h = 4;         
+let i = g / h;
+console.log(i)
+
+let j = 12;         
+let k = 10;         
+let l = j % k;
+console.log(l)
+
+let age = ("42")
+console.log(age);
